3946b1831e23d037057bb7c0486bcae8005511fd
MatthiasBakken <matthias.bakken@gmail.com>
2021-03-27 05:02:52 -0700

    completed project without stretch

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,12 +4,18 @@ import './Posts.css';
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+  console.log(props);
+  const { details, updateLikes } = props;
 
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {
+        details ? details.map(post => {
+          return (<Post post={post} key={post.id} likePost={updateLikes} />)
+        }) : null
+      }
     </div>
   );
 };
